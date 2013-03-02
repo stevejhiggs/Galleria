739f8fba890723da510ef0a666ce1a4fb97ded03
steve higgs <stevejhiggs@gmail.com>
2013-03-02 11:20:29 +0000

    start implementing editing

---
+++ Galleria/Controllers/ImageDetailsController.cs
@@ -0,0 +1,22 @@
+﻿using AutoMapper;
+using Galleria.Models;
+using Galleria.RavenDb.Controllers;
+using Galleria.ViewModels;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace Galleria.Controllers
+{
+    public class ImageDetailsController : RavenBaseController
+    {
+        //
+        // GET: /ImageDetails/Details/5
+
+        public ActionResult Edit(string id)
+        {
+            var storedImage = RavenSession.Query<StoredImage>().Where(a => a.Id == id);
+            return View("Partials/EditForm", storedImage);
+        }
+
+    }
+}
