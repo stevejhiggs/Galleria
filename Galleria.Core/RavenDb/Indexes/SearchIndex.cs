02abd14f317d59b4f42abeebfc3b1226a28018c6
steve higgs <stevejhiggs@gmail.com>
2013-04-07 12:51:16 +0100

    search controller works

---
+++ Galleria.Core/RavenDb/Indexes/SearchIndex.cs
@@ -0,0 +1,36 @@
+﻿using Galleria.Core.Models;
+using Raven.Abstractions.Indexing;
+using Raven.Client.Indexes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Galleria.Core.RavenDb.Indexes
+{
+    public class SearchIndex : AbstractIndexCreationTask<StoredImage, RavenDb.Indexes.SearchIndex.ReduceResult>
+    {
+        public class ReduceResult
+        {
+            public object[] SearchQuery { get; set; }
+            public DateTime UploadDateTime { get; set; }
+        }
+
+        public SearchIndex()
+        {
+            Map = images => 
+                from i in images
+                select new
+                {
+                    SearchQuery = i.Tags.Concat(new []
+                    {
+                        i.Name
+                    }),
+                    UploadDateTime = i.UploadDateTime
+                };
+
+            Index(x => x.SearchQuery, FieldIndexing.Analyzed);
+        }
+    }
+}
---
+++ Galleria/Controllers/Api/ImageCollectionController.cs
@@ -0,0 +1,43 @@
+﻿using AutoMapper;
+using Galleria.Core.Models;
+using Galleria.Core.RavenDb.Indexes;
+using Galleria.RavenDb.BaseControllers;
+using Galleria.ViewModels;
+using Raven.Client;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Galleria.Controllers
+{
+    public class SearchController : RavenBaseApiController
+    {
+        public async Task<IEnumerable<ProcessedImageViewModel>> Get(string searchText = null)
+        {
+            IList<StoredImage> results = null;
+
+            if (!string.IsNullOrWhiteSpace(searchText))
+            {
+                results = await RavenSession.Query<SearchIndex.ReduceResult, SearchIndex>()
+                   .Where(x => x.SearchQuery == (object)searchText)
+                   .As<StoredImage>()
+                    .ToListAsync();
+            }
+            else
+            {
+                //all images
+                results = await RavenSession.Query<StoredImage>().OrderByDescending(i => i.UploadDateTime).Take(1000).ToListAsync();
+            }
+
+            IList<ProcessedImageViewModel> existingMedia = new List<ProcessedImageViewModel>();
+            foreach (var upload in results)
+            {
+                var item = Mapper.Map<ProcessedImageViewModel>(upload);
+                item.LazyLoadPlaceholderUrl = "/Content/Images/ImagePlaceholder.png";
+                existingMedia.Add(item);
+            }
+
+            return existingMedia;
+        }
+    }
+}
