dd44af38120d543efb63ee69a87269284ca6673c
steve higgs <stevejhiggs@gmail.com>
2013-04-08 19:24:56 +0100

    up analysis to extended and remember to dispose of the tag extractor

--- Galleria.Core/ImageProcessing/TaglibExtractor.cs
+++ Galleria.Core/ImageProcessing/TaglibExtractor.cs
@@ -5,13 +5,15 @@ namespace Galleria.Core.ImageProcessing
     {
         public ExtractedImageInformation ExtractTags(string sourcePath, ExtractedImageInformation info)
         {
-            var imageFile = TagLib.File.Create(sourcePath) as TagLib.Image.File;
-            info.Orientation = (int)imageFile.ImageTag.Orientation;
-            info.Latitude = imageFile.ImageTag.Latitude;
-            info.Longitude = imageFile.ImageTag.Latitude;
-            info.Timestamp = imageFile.ImageTag.DateTime;
-            info.Name = imageFile.ImageTag.Title;
-            return info;
+            using (var imageFile = TagLib.File.Create(sourcePath) as TagLib.Image.File)
+            {
+                info.Orientation = (int)imageFile.ImageTag.Orientation;
+                info.Latitude = imageFile.ImageTag.Latitude;
+                info.Longitude = imageFile.ImageTag.Latitude;
+                info.Timestamp = imageFile.ImageTag.DateTime;
+                info.Name = imageFile.ImageTag.Title;
+                return info;
+            }
         }
     }
 }
